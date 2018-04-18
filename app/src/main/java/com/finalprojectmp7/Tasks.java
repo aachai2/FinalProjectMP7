5e560f94a2831433de82a36f94f5915100581e31
aachai2 <aachai2@illinois.edu>
2018-04-18 14:17:00 -0500

    Initial commit

---
+++ app/src/main/java/com/finalprojectmp7/Tasks.java
@@ -0,0 +1,34 @@
+package com.finalprojectmp7;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+//import com.android.volley.NetworkResponse;
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.ImageRequest;
+//import com.android.volley.toolbox.StringRequest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tasks {
+
+    private static final String SUBSCRIPTION_KEY = BuildConfig.API_KEY;
+
+
+}
